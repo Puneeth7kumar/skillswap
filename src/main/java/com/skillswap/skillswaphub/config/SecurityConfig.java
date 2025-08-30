f44feb4f8759480af072df10a645cd6a24953ee6
Puneeth7kumar <puneethpandith@gmail.com>
2025-08-30 16:13:13 +0530

    updated security config

--- src/main/java/com/skillswap/skillswaphub/config/SecurityConfig.java
+++ src/main/java/com/skillswap/skillswaphub/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://skillswap-tjmj.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
